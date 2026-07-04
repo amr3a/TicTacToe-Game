f0b2129e4a67402da155139a748bd7f7c52c0ad6
amr <amralamrei65@gmail.com>
2026-07-04 06:06:26 +0300

    create(Feat) Drwaing the Grid of the Game and Make it Functional To just change to X or to O

---
+++ frmMain.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Tic_Tac_Toe_Game
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCellContainer = new System.Windows.Forms.Label();
+            this.pbPicture1 = new System.Windows.Forms.PictureBox();
+            this.pbPicture2 = new System.Windows.Forms.PictureBox();
+            this.pbPicture3 = new System.Windows.Forms.PictureBox();
+            this.pbPicture6 = new System.Windows.Forms.PictureBox();
+            this.pbPicture5 = new System.Windows.Forms.PictureBox();
+            this.pbPicture4 = new System.Windows.Forms.PictureBox();
+            this.pbPicture9 = new System.Windows.Forms.PictureBox();
+            this.pbPicture8 = new System.Windows.Forms.PictureBox();
+            this.pbPicture7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCellContainer
+            // 
+            this.lblCellContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCellContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCellContainer.Location = new System.Drawing.Point(248, 100);
+            this.lblCellContainer.Name = "lblCellContainer";
+            this.lblCellContainer.Size = new System.Drawing.Size(366, 365);
+            this.lblCellContainer.TabIndex = 0;
+            this.lblCellContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.lblCellContainer_Paint);
+            // 
+            // pbPicture1
+            // 
+            this.pbPicture1.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture1.Location = new System.Drawing.Point(259, 116);
+            this.pbPicture1.Name = "pbPicture1";
+            this.pbPicture1.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture1.TabIndex = 1;
+            this.pbPicture1.TabStop = false;
+            this.pbPicture1.Click += new System.EventHandler(this.pbPicture1_Click);
+            // 
+            // pbPicture2
+            // 
+            this.pbPicture2.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture2.Location = new System.Drawing.Point(388, 116);
+            this.pbPicture2.Name = "pbPicture2";
+            this.pbPicture2.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture2.TabIndex = 2;
+            this.pbPicture2.TabStop = false;
+            this.pbPicture2.Click += new System.EventHandler(this.pbPicture2_Click);
+            // 
+            // pbPicture3
+            // 
+            this.pbPicture3.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture3.Location = new System.Drawing.Point(510, 116);
+            this.pbPicture3.Name = "pbPicture3";
+            this.pbPicture3.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture3.TabIndex = 3;
+            this.pbPicture3.TabStop = false;
+            this.pbPicture3.Click += new System.EventHandler(this.pbPicture3_Click);
+            // 
+            // pbPicture6
+            // 
+            this.pbPicture6.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture6.Location = new System.Drawing.Point(513, 224);
+            this.pbPicture6.Name = "pbPicture6";
+            this.pbPicture6.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture6.TabIndex = 6;
+            this.pbPicture6.TabStop = false;
+            this.pbPicture6.Click += new System.EventHandler(this.pbPicture6_Click);
+            // 
+            // pbPicture5
+            // 
+            this.pbPicture5.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture5.Location = new System.Drawing.Point(391, 224);
+            this.pbPicture5.Name = "pbPicture5";
+            this.pbPicture5.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture5.TabIndex = 5;
+            this.pbPicture5.TabStop = false;
+            this.pbPicture5.Click += new System.EventHandler(this.pbPicture5_Click);
+            // 
+            // pbPicture4
+            // 
+            this.pbPicture4.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture4.Location = new System.Drawing.Point(262, 224);
+            this.pbPicture4.Name = "pbPicture4";
+            this.pbPicture4.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture4.TabIndex = 4;
+            this.pbPicture4.TabStop = false;
+            this.pbPicture4.Click += new System.EventHandler(this.pbPicture4_Click);
+            // 
+            // pbPicture9
+            // 
+            this.pbPicture9.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture9.Location = new System.Drawing.Point(513, 338);
+            this.pbPicture9.Name = "pbPicture9";
+            this.pbPicture9.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture9.TabIndex = 9;
+            this.pbPicture9.TabStop = false;
+            this.pbPicture9.Click += new System.EventHandler(this.pbPicture9_Click);
+            // 
+            // pbPicture8
+            // 
+            this.pbPicture8.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture8.Location = new System.Drawing.Point(391, 338);
+            this.pbPicture8.Name = "pbPicture8";
+            this.pbPicture8.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture8.TabIndex = 8;
+            this.pbPicture8.TabStop = false;
+            this.pbPicture8.Click += new System.EventHandler(this.pbPicture8_Click);
+            // 
+            // pbPicture7
+            // 
+            this.pbPicture7.Image = global::Tic_Tac_Toe_Game.Properties.Resources.QM;
+            this.pbPicture7.Location = new System.Drawing.Point(262, 338);
+            this.pbPicture7.Name = "pbPicture7";
+            this.pbPicture7.Size = new System.Drawing.Size(80, 80);
+            this.pbPicture7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPicture7.TabIndex = 7;
+            this.pbPicture7.TabStop = false;
+            this.pbPicture7.Click += new System.EventHandler(this.pbPicture7_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 528);
+            this.Controls.Add(this.pbPicture9);
+            this.Controls.Add(this.pbPicture8);
+            this.Controls.Add(this.pbPicture7);
+            this.Controls.Add(this.pbPicture6);
+            this.Controls.Add(this.pbPicture5);
+            this.Controls.Add(this.pbPicture4);
+            this.Controls.Add(this.pbPicture3);
+            this.Controls.Add(this.pbPicture2);
+            this.Controls.Add(this.pbPicture1);
+            this.Controls.Add(this.lblCellContainer);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toe Game";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPicture7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCellContainer;
+        private System.Windows.Forms.PictureBox pbPicture1;
+        private System.Windows.Forms.PictureBox pbPicture2;
+        private System.Windows.Forms.PictureBox pbPicture3;
+        private System.Windows.Forms.PictureBox pbPicture6;
+        private System.Windows.Forms.PictureBox pbPicture5;
+        private System.Windows.Forms.PictureBox pbPicture4;
+        private System.Windows.Forms.PictureBox pbPicture9;
+        private System.Windows.Forms.PictureBox pbPicture8;
+        private System.Windows.Forms.PictureBox pbPicture7;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tic_Tac_Toe_Game
+{  
+ 
+
+
+    public partial class frmMain : Form
+    {
+        public enum enPlayerType
+        {
+            QM,
+            X,
+            O
+        }
+        public enum enPlayerName
+        {
+            Player1,
+            Player2,
+            Computer,
+            UnKnownPlayer
+        }
+        public enum enPlayerState
+        {
+            Active,
+            Unactive
+        }
+        private struct Player
+        {
+           public enPlayerName playerName;
+           public enPlayerType playerType;
+           public enPlayerState playerState;
+
+        
+            public string PlayerName()
+            {
+                switch (playerName)
+                {
+                    case enPlayerName.Player1:
+                        return "Player 1";
+                    case enPlayerName.Player2:
+                        return "Player 2";
+                    case enPlayerName.Computer:
+                        return "Computer";
+                    default:
+                        return "Unknown Player";
+                }
+            }
+
+            private Image PictureBoxImage()
+            {
+                switch (playerType)
+                {
+                    case enPlayerType.X:
+                       return Properties.Resources.X;
+                    case enPlayerType.O:
+                        return Properties.Resources.O;
+                    default:
+                        return Properties.Resources.QM;
+                }
+            }
+            private string ConvertPlayerTypeToSTring()
+            {
+                switch(playerType)
+                {
+                    case enPlayerType.X:
+                        return "X";
+                    case enPlayerType.O:
+                        return "O";
+
+                    default:
+                        return "QM";
+                }
+            }
+            public PictureBox UpdatePictureBox(PictureBox pictureBox)
+            {
+                string PlayerTypeString = ConvertPlayerTypeToSTring();
+                pictureBox.Tag = PlayerTypeString;
+                pictureBox.Image = PictureBoxImage();
+                return pictureBox;
+            }
+
+
+        }
+        private Player firstPlayer = new Player();
+        private Player secondPlayer = new Player();
+        public frmMain()
+        {
+            firstPlayer.playerName = enPlayerName.Player1;
+            firstPlayer.playerType = enPlayerType.X;
+            firstPlayer.playerState = enPlayerState.Active;
+
+            secondPlayer.playerName = enPlayerName.Player2;
+            secondPlayer.playerType = enPlayerType.O;
+            secondPlayer.playerState = enPlayerState.Unactive;
+            InitializeComponent();
+        }
+        private void initializeCellContainer()
+        {
+            lblCellContainer.Width = 260;
+            lblCellContainer.Height = 260;
+          
+        }
+        private void SetPicturesInGrid()
+        {
+            int cellSize = lblCellContainer.Width / 3;
+            int cellDoubleSize = cellSize + cellSize;
+            int ExtraDistance = 13;
+            pbPicture1.Location = new Point(lblCellContainer.Location.X + ExtraDistance, lblCellContainer.Location.Y + ExtraDistance);
+            pbPicture2.Location = new Point(lblCellContainer.Location.X + cellSize + ExtraDistance, lblCellContainer.Location.Y + ExtraDistance);
+            pbPicture3.Location = new Point(lblCellContainer.Location.X + cellDoubleSize + ExtraDistance, lblCellContainer.Location.Y + ExtraDistance);
+
+            pbPicture4.Location = new Point(lblCellContainer.Location.X + ExtraDistance, lblCellContainer.Location.Y + cellSize + ExtraDistance);
+            pbPicture5.Location = new Point(lblCellContainer.Location.X + cellSize + ExtraDistance, lblCellContainer.Location.Y + cellSize + ExtraDistance);
+            pbPicture6.Location = new Point(lblCellContainer.Location.X + cellDoubleSize + ExtraDistance, lblCellContainer.Location.Y + cellSize + ExtraDistance);
+
+            pbPicture7.Location = new Point(lblCellContainer.Location.X + ExtraDistance, lblCellContainer.Location.Y + cellDoubleSize + ExtraDistance);
+            pbPicture8.Location = new Point(lblCellContainer.Location.X + cellSize + ExtraDistance, lblCellContainer.Location.Y + cellDoubleSize + ExtraDistance);
+            pbPicture9.Location = new Point(lblCellContainer.Location.X + cellSize + cellSize + ExtraDistance, lblCellContainer.Location.Y + cellDoubleSize + ExtraDistance);
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+            initializeCellContainer();
+            SetPicturesInGrid();
+
+        }
+
+     
+        private void DrawGrid(PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            Graphics drawLine = e.Graphics;
+
+            float cellSize = lblCellContainer.Width / 3f;
+            PointF verticalPoint1;
+            PointF verticalPoint2;
+
+            PointF horizontalPoint1;
+            PointF horizontalPoint2;
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                verticalPoint1 = new PointF(cellSize, 0.0f);
+                verticalPoint2 = new PointF(cellSize, lblCellContainer.Height);
+
+                horizontalPoint1 = new PointF(0.0f, cellSize);
+                horizontalPoint2 = new PointF(lblCellContainer.Width, cellSize);
+
+                drawLine.DrawLine(pen, verticalPoint1, verticalPoint2);
+                drawLine.DrawLine(pen, horizontalPoint1, horizontalPoint2);
+                cellSize += cellSize;
+            }
+        }
+        private void lblCellContainer_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGrid(e);
+           
+        }
+        private PictureBox UpdatePictureCell(PictureBox pictureBox)
+        {
+            if(firstPlayer.playerState == enPlayerState.Active)
+            {
+                firstPlayer.playerState = enPlayerState.Unactive;
+                secondPlayer.playerState = enPlayerState.Active;
+                return  firstPlayer.UpdatePictureBox(pictureBox);
+                
+            }
+            else
+            {
+                secondPlayer.playerState = enPlayerState.Unactive;
+                firstPlayer.playerState = enPlayerState.Active;
+
+                return secondPlayer.UpdatePictureBox(pictureBox);
+            }
+        }
+        private void pbPicture1_Click(object sender, EventArgs e)
+        {
+            pbPicture1 = UpdatePictureCell(pbPicture1);
+        }
+
+        private void pbPicture2_Click(object sender, EventArgs e)
+        {
+            pbPicture2 = UpdatePictureCell(pbPicture2);
+        }
+
+        private void pbPicture3_Click(object sender, EventArgs e)
+        {
+            pbPicture3 = UpdatePictureCell(pbPicture3);
+        }
+
+        private void pbPicture4_Click(object sender, EventArgs e)
+        {
+            pbPicture4 = UpdatePictureCell(pbPicture4);
+        }
+
+        private void pbPicture5_Click(object sender, EventArgs e)
+        {
+            pbPicture5 = UpdatePictureCell(pbPicture5);
+        }
+
+        private void pbPicture6_Click(object sender, EventArgs e)
+        {
+            pbPicture6 = UpdatePictureCell(pbPicture6);
+        }
+
+        private void pbPicture7_Click(object sender, EventArgs e)
+        {
+            pbPicture7 = UpdatePictureCell(pbPicture7);
+        }
+
+        private void pbPicture8_Click(object sender, EventArgs e)
+        {
+            pbPicture8 = UpdatePictureCell(pbPicture8);
+        }
+
+        private void pbPicture9_Click(object sender, EventArgs e)
+        {
+            pbPicture9 = UpdatePictureCell(pbPicture9);
+        }
+
+    }
+}
